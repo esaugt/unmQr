930b72ad37dd3bee4fd0c834d4c1e7f0e10e55fe
Esaú Gamboa <esagambo@hotmail.com>
2023-08-03 01:23:40 -0500

    cambio linea postgres

--- unmQr/settings.py
+++ unmQr/settings.py
@@ -84,7 +84,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgres://user:password@localhost:5432/mydatabase',
         conn_max_age=600
     )
 }
